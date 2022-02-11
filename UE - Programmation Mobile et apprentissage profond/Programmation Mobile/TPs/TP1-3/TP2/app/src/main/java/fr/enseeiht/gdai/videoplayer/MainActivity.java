94fa563f889cb049d9bec9837e6390dea6c6451e
Guohao <dave980126@outlook.com>
2022-02-11 12:04:30 +0100

    update

--- UE - Programmation Mobile et apprentissage profond/Programmation Mobile/TPs/TP1-3/TP2/app/src/main/java/fr/enseeiht/gdai/videoplayer/MainActivity.java
+++ UE - Programmation Mobile et apprentissage profond/Programmation Mobile/TPs/TP1-3/TP2/app/src/main/java/fr/enseeiht/gdai/videoplayer/MainActivity.java
@@ -1,14 +1,103 @@
 package fr.enseeiht.gdai.videoplayer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.SurfaceHolder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnSelect;
+    private Button btnPlay;
+    private Button btnRestart;
+    private Button btnPause;
+    private TextView URIPath;
+    private VideoView videoView;
+
+    private static final int SELECT_VIDEO = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSelect = (Button) findViewById(R.id.btnSelect);
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnRestart = (Button) findViewById(R.id.btnRestart);
+        btnPause = (Button) findViewById(R.id.btnPause);
+        URIPath = (TextView) findViewById(R.id.tvURIPath);
+        videoView = (VideoView) findViewById(R.id.videoView);
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setVideo();
+            }
+        });
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                videoView.start();
+            }
+        });
+
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                videoView.start();
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if videoView.
+                videoView.pause();
+            }
+        });
+    }
+
+    private void setVideo() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+        intent.setDataAndType(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, "video/*");
+        startActivityForResult(Intent.createChooser(intent,"选择视频"), SELECT_VIDEO);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_VIDEO && resultCode == RESULT_OK) {
+            Uri selectVideo = data.getData();
+            videoView.setVideoURI(selectVideo);
+            videoView.requestFocus();
+            URIPath.setText(selectVideo.toString());
+        }
+    }
+
+}
+
+public class PlayerActivity extends Activity implements SurfaceHolder.Callback{
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
     }
 }
