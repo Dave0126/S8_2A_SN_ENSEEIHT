d96e071674a72c9376e3027f52696319f0fe2922
Guohao <dave980126@outlook.com>
2022-02-18 08:18:17 +0100

    update

--- UE - Paradigmes émergents de la programmation/Programmation Avancée/TPs/TP01/TP01/fn_param_multiples.py
+++ UE - Paradigmes émergents de la programmation/Programmation Avancée/TPs/TP01/TP01/fn_param_multiples.py
@@ -2,6 +2,8 @@ def f(a=4, *p, x, **kw):
     print('a =', a)
     print('p =', p)
     print('kw =', kw)
+    print('x =', x)
     print(*p, sep='... ', end=' !\n')
 
 f(1, 2, 3, x=5, y=6, z=7)
+
---
+++ UE - Paradigmes émergents de la programmation/Programmation Avancée/TPs/TP01/test.py
@@ -0,0 +1,2 @@
+l = [2 * i + 5 for i in range(2, 10)]
+l
