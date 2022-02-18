0c2ed277554fd3d4365ccde0552b6ad9381b58cf
gdai_on_linux_n7 <gdai@kepler.enseeiht.fr>
2022-02-18 09:57:29 +0100

    add tp2 of openMP

--- UE - Applications concurrentes et communicantes/OpenMP/TPs/TP2/TP_tree/treetrav_par.c
+++ UE - Applications concurrentes et communicantes/OpenMP/TPs/TP2/TP_tree/treetrav_par.c
@@ -11,19 +11,29 @@
    recursive tree traversal. */
 
 void treetraverse_par(TreeNode *root, int nth){
+#pragma omp parallel num_threads (nth)
+   {
+#pragma omp single
+      {
+         printf("< treetraverse_par >: Number of current thread: %d\n",omp_get_thread_num() );
 
-  int depth, i;
+         int depth, i;
 
-  /* To be used in the second part of the excercise */
-  depth = 0;
+         /* To be used in the second part of the excercise */
+         // depth = 0;
+         depth = 1 + log(nth);
 
-  /* Initialize the counter to 0. MAXTHREADS is the maximum number of
-     allowed threads. */
-  for(i=0; i<MAXTHREADS; i++)
-    countnodes[i]=0;
+         /* Initialize the counter to 0. MAXTHREADS is the maximum number of
+            allowed threads. */
+         for(i=0; i<MAXTHREADS; i++)
+            countnodes[i]=0;
 
-  /* Start the tree traversal by calling the recursive routine */
-  treetraverserec_par(root, depth);
+         /* Start the tree traversal by calling the recursive routine */
+         treetraverserec_par(root, depth);
+      }
+      
+   }
+
   
   return;
 
@@ -50,20 +60,28 @@ void treetraverserec_par(TreeNode *root, int depth){
     /* If this node is not a leaf...*/
 
     /* ...visit the left subtree... */
-    treetraverserec_par(root->left, depth);
-
-    /* ...visit the right subtree... */
-    treetraverserec_par(root->right, depth);
+#pragma omp task if (root->l >= depth)
+      {
+         treetraverserec_par(root->left, depth);
+      }
+    
+#pragma omp task if (root->l >= depth)
+      {
+         /* ...visit the right subtree... */
+         treetraverserec_par(root->right, depth);
+      }
     
     /* ...compute root->v as the sum of the v values on the left and
        right children... */
-    root->v += (root->right)->v + (root->left)->v;
+#pragma omp taskwait
+         root->v += (root->right)->v + (root->left)->v;
   }
 
   /* ...add root->n to root->v... */
   root->v += root->n;
   
   /* ...do some random work... */
+
   for(it=0; it<NIT; it++)
     for(i=1; i<DATASIZE; i++)
       root->data[0] += root->data[i];
@@ -71,11 +89,9 @@ void treetraverserec_par(TreeNode *root, int depth){
   
   /* ...increment the counter of the number of nodes treated by the
      executing thread. */
-  iam = 0;
+  iam = omp_get_thread_num();
   countnodes[iam] +=1;
 
   return;
 
 }
-
-
