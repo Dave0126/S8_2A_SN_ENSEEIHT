bdb0350cc7ee1b2c80648291f5bd288bd9409826
Guohao <dave980126@outlook.com>
2022-03-15 23:43:04 +0100

    ADD test of Examen_19 programmtion mobile

---
+++ UE - Programmation Mobile et apprentissage profond/Programmation Mobile/TPs/TP4-6/MyApplication/app/src/main/java/fr/enseeiht/gdai/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package fr.enseeiht.gdai.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MediaStore.EXTRA_OUTPUT, );
+    }
+}
